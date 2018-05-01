4239e7865db24f4649bfdc63fc8a1869bab19cdf
Rudolf Rosa <rosa@ufal.mff.cuni.cz>
2018-05-01 21:33:40 +0200

    first try

---
+++ metadata2commands.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+#coding: utf-8
+
+import json
+import sys
+
+inputDataset, outputDir = sys.argv[1:]
+
+with open(inputDataset+'/metadata.json', 'r') as metadata:
+    data = json.load(metadata)
+    for language in data:
+        model = 'udpipe-ud-2.0-170801/' + language['lcode'] + '_' + language['tcode'] + '.udpipe'
+
+        catinput  = 'cat ' + inputDataset + '/' + language['psegmorfile'] + ' |' 
+        runudpipe = './udpipe --tag --parse ' + model
+        diroutput = '> ' + outputDir + '/' + language['outfile']
+
+        print(catinput, runudpipe, diroutput)
+
+
