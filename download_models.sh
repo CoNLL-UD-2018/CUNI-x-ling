f71d01df5f3350d6fc6161fdc8fdf7bc3e319629
Rudolf Rosa <rosa@ufal.mff.cuni.cz>
2018-06-27 03:17:48 +0200

    set defaults during download

--- download_models.sh
+++ download_models.sh
@@ -9,3 +9,17 @@ do
     wget ${url}${f}
 done
 
+
+
+# default models for the langs (for cross-lingual stuff
+# and for test treebanks without training data)
+# e.g. cs.sup.udpipe -> cs_pdt.sup.udpipe
+
+for d in $(cat ../defaults.list)
+do
+    for f in $d.*
+    do
+        ln -s $f $(echo $f | sed 's/_[a-z]*//')
+    done
+done
+
