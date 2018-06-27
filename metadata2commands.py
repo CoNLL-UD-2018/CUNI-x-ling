8ca758492aca0cd7495728dacabd768a7c537d98
Rudolf Rosa <rosa@ufal.mff.cuni.cz>
2018-06-27 03:33:56 +0200

    logistics to handle defaults and specials

--- metadata2commands.py
+++ metadata2commands.py
@@ -3,18 +3,46 @@
 
 import json
 import sys
+import os.path
+
+# raw input text:
+#   language['rawfile']
+# segmented, tokenized and morphotagged data (with baseline models):
+#   language['psegmorfile']
+
 
 inputDataset, outputDir = sys.argv[1:]
+
+def sup(lcode, tcode=None):
+    if (tcode is not None):
+        return 'CUNI-x-ling/models/' + lcode + '_' + tcode + '.sup.udpipe'
+    else:
+        return 'CUNI-x-ling/models/' + lcode + '.sup.udpipe'
+
+def run(lcode):
+    return 'CUNI-x-ling/run/' + lcode + '.sh'
+
+udpipe = './udpipe --tokenize --tag --parse '
 
 with open(inputDataset+'/metadata.json', 'r') as metadata:
     data = json.load(metadata)
     for language in data:
-        model = 'udpipe-ud-2.0-170801/' + language['lcode'] + '_' + language['tcode'] + '.udpipe'
 
-        # catinput  = 'cat ' + inputDataset + '/' + language['psegmorfile'] + ' |' 
+        # cat the input file
         catinput  = 'cat ' + inputDataset + '/' + language['rawfile'] + ' |' 
-        # runudpipe = './udpipe --tag --parse ' + model
-        runudpipe = './udpipe --tokenize --tag --parse ' + model
+
+        # run udpipe
+        if os.path.isfile(run(lcode)):
+            # for special targets, there is a file with the commands to run
+            runudpipe = 5
+        elif os.path.isfile( sup(language['lcode'], language['tcode']) ):
+            # for most targets, there is the lcode_tcode model
+            runudpipe = udpipe + sup(language['lcode'], language['tcode'])
+        else:
+            # if not, use the default model for the lcode
+            runudpipe = udpipe + sup(language['lcode'])
+
+        # save the output
         diroutput = '> ' + outputDir + '/' + language['outfile']
 
         print(catinput, runudpipe, diroutput)
