c624524d246e24428a29e3ee89f246fe1d682323
Rudolf Rosa <rosa@ufal.mff.cuni.cz>
2018-06-28 11:35:01 +0200

    th tok

--- run/th.sh
+++ run/th.sh
@@ -2,5 +2,7 @@
 
 # TODO translate vi+zh+id->th, train standard udpipe
 # beware: only id has reasonable tokenization
-./udpipe --tokenize --tag --parse CUNI-x-ling/models/vi.sup.udpipe
+./udpipe --tokenize CUNI-x-ling/models/th.tok.udpipe | \
+    ./udpipe --tag --parse CUNI-x-ling/models/id.sup.udpipe
 
+# unidecode | \
