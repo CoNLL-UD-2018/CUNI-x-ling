2a3d863bd6066fe0da631b01b2bebb8928d83f6c
Rudolf Rosa <rosa@ufal.mff.cuni.cz>
2018-06-30 00:20:46 +0200

    th: mixing based on mono las

--- run/th.sh
+++ run/th.sh
@@ -4,19 +4,19 @@ cd ~/CUNI-x-ling/
 
 tools/udpipe --tokenize models/th.tok.udpipe > th.tok
 
-cat th.tok | tools/udpipe --tag --parse models/vi.trans_emb.th.udpipe > th.vi
 cat th.tok | tools/udpipe --tag --parse models/id.trans_emb.th.udpipe > th.id
 cat th.tok | tools/udpipe --tag --parse models/zh.trans_emb.th.udpipe > th.zh
+cat th.tok | tools/udpipe --tag --parse models/vi.trans_emb.th.udpipe > th.vi
 
-# weights based on wals sim
+# weights based on mono LAS
 # tree and deprel
-tools/treecomb.sh th.vi th.id th.zh 75 64 60 > th.1
+tools/treecomb.sh th.id th.zh th.vi 75 55 40 > th.1
 # lemma
-tools/labelcomb_weighted.py 3 th.1 th.id th.zh 75 64 60 > th.2
+tools/labelcomb_weighted.py 3 th.1 th.zh th.vi 75 55 40 > th.2
 # POS
-tools/labelcomb_weighted.py 4 th.2 th.id th.zh 75 64 60 > th.3
+tools/labelcomb_weighted.py 4 th.2 th.zh th.vi 75 55 40 > th.3
 # feats
-tools/labelcomb_weighted.py 6 th.3 th.id th.zh 75 64 60 > th.4
+tools/labelcomb_weighted.py 6 th.3 th.zh th.vi 75 55 40 > th.4
 
 cat th.4
 
