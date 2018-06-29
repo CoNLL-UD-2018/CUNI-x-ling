65b523c708ad26f4a3b3da90e22a790964f22f55
Rudolf Rosa <rosa@ufal.mff.cuni.cz>
2018-06-30 00:09:43 +0200

    typo

--- run/pcm.sh
+++ run/pcm.sh
@@ -3,7 +3,7 @@
 cd ~/CUNI-x-ling/
 
 tools/udpipe --tokenize models/en.sup.udpipe | \
-    tools/copy_form_to_col8.py \
+    tools/copy_form_to_col8.py | \
     tools/translate_pcm_treebank.py en.dict | \
     #tools/translate_pcm_treebank_wiki.py en.dict | \
     tools/udpipe --tag --parse models/en.sup.udpipe | \
