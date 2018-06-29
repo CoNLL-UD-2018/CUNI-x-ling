af2c49510ce2568f0f4a78d23e3eb9bc18809abb
Rudolf Rosa <rosa@ufal.mff.cuni.cz>
2018-06-29 12:01:42 +0200

    bxr delex combo

--- run/bxr.sh
+++ run/bxr.sh
@@ -7,9 +7,15 @@ cd ~/CUNI-x-ling/
 tools/udpipe --tokenize models/bxr_bdt.sup.udpipe | \
     tools/copy_form_to_col8.py | \
     tools/devow_form.py | \
-    tools/udpipe --tag --parse models/bxr.devow.udpipe | \
+    tools/udpipe --tag \
+    > bxr.tag
+
+cat bxr.tag | tools/udpipe --parse models/bxr.devow.udpipe > bxr.bxr
+cat bxr.tag | tools/udpipe --parse models/hi.delex.udpipe > bxr.hi
+cat bxr.tag | tools/udpipe --parse models/ug.delex.udpipe > bxr.ug
+
+# weights based on delex LAS
+tools/treecomb.sh bxr.bxr bxr.hi bxr.ug 45 41 38 | \
     tools/copy_col8_to_form.py | \
     tools/copy_form_to_lemma.py -l
-
-
 
