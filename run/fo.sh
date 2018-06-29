82e090428e94acf074baec9dcea5c6acf74aef68
David Mareček <marecek@ufal.mff.cuni.cz>
2018-06-29 11:47:32 +0200

    merge morphofeatures

--- run/fo.sh
+++ run/fo.sh
@@ -16,10 +16,10 @@ cat fo.tok | \
     tools/devow_form.py | \
     tools/udpipe --tag models/no.devow.udpipe | \
     tools/copy_col8_to_form.py | \
+    tools/copy_form_to_lemma.py -l | \
     tools/fix_morphology_by_unimorph.py morpho/fo | \
     tools/copy_form_to_col8.py | \
     tools/devow_form.py | \
     tools/udpipe --parse models/no.devow.udpipe | \
-    tools/copy_col8_to_form.py | \
-    tools/copy_form_to_lemma.py -l
+    tools/copy_col8_to_form.py
 
