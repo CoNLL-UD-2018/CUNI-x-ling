d2b56124811be4845f983fc229d92ae620f34745
Rudolf Rosa <rosa@ufal.mff.cuni.cz>
2018-07-02 09:34:26 +0200

    pcm wiki-based

---
+++ metadata2commands_pcmwiki.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+#coding: utf-8
+
+import json
+import sys
+import os.path
+
+# raw input text:
+#   language['rawfile']
+# segmented, tokenized and morphotagged data (with baseline models):
+#   language['psegmorfile']
+
+
+inputDataset, outputDir = sys.argv[1:]
+
+# selectively running only for some languages
+# languages = None
+languages = set()
+languages.add('pcm')
+#languages.add('th')
+#languages.add('fo')
+#languages.add('br')
+#languages.add('bxr')
+#languages.add('kk')
+#languages.add('kmr')
+#languages.add('hsb')
+#languages.add('hy')
+
+# print(languages)
+
+def sup(lcode, tcode=None):
+    if (tcode is not None):
+        return 'CUNI-x-ling/models/' + lcode + '_' + tcode + '.sup.udpipe'
+    else:
+        return 'CUNI-x-ling/models/' + lcode + '.sup.udpipe'
+
+def run(lcode):
+    return 'CUNI-x-ling/run/' + lcode + 'wiki.sh'
+
+# udpipe_full = './udpipe --tokenize --tag --parse '
+udpipe_parse = './udpipe --parse '
+
+with open(inputDataset+'/metadata.json', 'r') as metadata:
+    data = json.load(metadata)
+    for language in data:
+
+        # selectively running only for some languages
+        if languages is not None and language['lcode'] not in languages:
+            continue
+
+        # Run udpipe
+        if os.path.isfile(run(language['lcode'])):
+            # For special targets, there is a file with the commands to run
+            # In such case, we process the raw data
+            catinput  = 'cat ' + inputDataset + '/' + language['rawfile'] + ' |' 
+            runudpipe = run(language['lcode'])
+        elif os.path.isfile( sup(language['lcode'], language['tcode']) ):
+            # For most targets, there is the lcode_tcode model
+            # We do only parsing
+            catinput  = 'cat ' + inputDataset + '/' + language['psegmorfile'] + ' |' 
+            runudpipe = udpipe_parse + sup(language['lcode'], language['tcode'])
+        else:
+            # If not, use the default model for the lcode
+            # We do only parsing
+            catinput  = 'cat ' + inputDataset + '/' + language['psegmorfile'] + ' |' 
+            runudpipe = udpipe_parse + sup(language['lcode'])
+
+        # save the output
+        diroutput = '> ' + outputDir + '/' + language['outfile']
+
+        print(catinput, runudpipe, diroutput)
+
+
---
+++ run/pcmwiki.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# TODO: monotranslate input to en (use en_wtb forms as vocabulary corpus?)
+
+cd ~/CUNI-x-ling/
+
+tools/udpipe --tokenize models/en.sup.udpipe | \
+    tools/copy_form_to_col8.py \
+    > pcm.tok
+
+    # tools/words2freqlist_simple_tb.py pcm.tok pcm.freqlist
+    
+cat pcm.tok | \
+    #tools/translate_pcm_treebank.py en.dict | \
+    tools/translate_pcm_treebank_wiki.py en.dict | \
+    tools/udpipe --tag --parse models/en.sup.udpipe | \
+    tools/copy_col8_to_form.py | \
+    tools/copy_form_to_lemma.py -l -s
+
